16b5434e2d7d250c3f57421e0a8f197b7a596bb4
github username <github email address>
2019-09-19 15:21:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log('%c HI', 'color: firebrick')
+
+getDoggos();
+
+function getDoggos() {
+    dogs = fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(data => addImages(data))
+    return dogs
+}
+
+// fetch("https://dog.ceo/api/breeds/image/random/4")
+//     .then((response) => response.json())
+//     .then(data => addImages(data))
+
+function addImages(data) {
+    const imgLinks = data["message"]
+    const container = document.querySelector("div#dog-image-container")
+
+    imgLinks.forEach(element => {
+        container.insertAdjacentHTML('beforeend', `<img src= ${element}></img>` )
+    });
+}
+
+// function addImages(data) {
+//     const container = document.querySelector("#dog-image-container")
+//     myLis = data["message"]
+//     myLis.forEach((li) => {
+//         container.insertAdjacentHTML('beforeend', `<img src= ${li}></img>`)
+//     })
+// }
